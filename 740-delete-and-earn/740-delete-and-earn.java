e3560bed45e1f398471c1528489c1d1f1bf360db
Shubham Prakash <43783336+Shub4am@users.noreply.github.com>
2022-03-18 12:41:04 +0530

    Time: 17 ms (6.64%), Space: 48 MB (7.14%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -1,19 +1,29 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
-        if(nums.length == 1) 
-            return nums[0];
-        int[] n = new int[1001] ;
-        for(int i: nums)
-            n[i] += i;
-        
-        int twoBack = n[0];
-        int oneBack = Math.max(twoBack, n[1]);
-        int temp = oneBack;
-        for(int i = 2; i < n.length; i++){
-            temp = Math.max(n[i] + twoBack, oneBack);
-            twoBack = oneBack;
-            oneBack = temp;
+        int maxNumber = 0;
+        HashMap<Integer, Integer> points = new HashMap<>();
+        for (int num : nums) {
+            points.put(num, points.getOrDefault(num, 0) + num);
+            maxNumber = Math.max(maxNumber, num);
         }
-        return temp;
+        
+        List<Integer> elements = new ArrayList<Integer>(points.keySet());
+        Collections.sort(elements);
+        int twoBack = 0;
+        int oneBack = points.get(elements.get(0));
+        
+        for (int i = 1; i < elements.size(); i++) {
+            int currentElement = elements.get(i);
+            int temp = oneBack;
+            if (currentElement == elements.get(i - 1) + 1) {
+                oneBack = Math.max(oneBack, twoBack + points.get(currentElement));
+            } else {
+                oneBack += points.get(currentElement);
+            }
+
+            twoBack = temp;
+        }
+        
+        return oneBack;
     }
 }
