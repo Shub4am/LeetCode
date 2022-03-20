9095d29bafc946223ab64f07712e76b94e6a6fd0
Shubham Prakash <43783336+Shub4am@users.noreply.github.com>
2022-03-20 14:44:10 +0530

    Time: 4 ms (90.54%), Space: 49.8 MB (86.82%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
@@ -1,15 +1,20 @@
-
 class Solution {
     public int minDominoRotations(int[] tops, int[] bottoms) {
-        HashSet<Integer> s = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6));
-        int[] countTops = new int[7], countBottoms = new int[7];
-        for (int i = 0; i < tops.length; ++i) {
-            s.retainAll(new HashSet<>(Arrays.asList(tops[i], bottoms[i])));
-            countTops[tops[i]]++;
-            countBottoms[bottoms[i]]++;
+        int[] countTops = new int[7], countBottoms = new int[7], countSame = new int[7];
+        int n = tops.length;
+        for (int i = 0; i < n; i++) {
+            int a = tops[i], b = bottoms[i];
+            countTops[a] += 1;
+            countBottoms[b] += 1;
+            if (a == b) countSame[a] += 1;
         }
-        for (int i : s)
-            return tops.length - Math.max(countTops[i], countBottoms[i]);
-        return -1;
+        int res = n;
+        for (int v = 1; v <= 6; v++) {
+            if (countTops[v] + countBottoms[v] - countSame[v] == n) {
+                int minSwap = Math.min(countTops[v], countBottoms[v]) - countSame[v];
+                res = Math.min(res, minSwap);
+            }
+        }   
+        return res == n ? -1 : res;
     }
 }
