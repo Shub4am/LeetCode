ce7cc7c2bbacec8b9607d195ff60e1d1f08124a1
Shubham Prakash <43783336+Shub4am@users.noreply.github.com>
2022-04-10 06:31:37 +0530

    Time: 474 ms (5.16%), Space: 163.5 MB (5.16%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,20 +1,29 @@
 class Solution {
-    int dfs(List<List<Integer>> al, boolean[] visited, int from) {
-    int change = 0;
-    visited[from] = true;
-    for (var to : al.get(from))
-        if (!visited[Math.abs(to)])
-            change += dfs(al, visited, Math.abs(to)) + (to > 0 ? 1 : 0);
-    return change;
-    }
-    public int minReorder(int n, int[][] connections) {
-    List<List<Integer>> al = new ArrayList<>();
-    for(int i = 0; i < n; ++i) 
-        al.add(new ArrayList<>());
-    for (var c : connections) {
-        al.get(c[0]).add(c[1]);
-        al.get(c[1]).add(-c[0]);
-    }
-    return dfs(al, new boolean[n], 0);
+    public int minReorder(int n, int[][] cs) {
+        Set<String> st = new HashSet<>();
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        for (int[] c : cs) {
+            st.add(c[0] + "," + c[1]);
+            map.computeIfAbsent(c[0], k -> new HashSet<>());
+            map.computeIfAbsent(c[1], k -> new HashSet<>());
+            map.get(c[0]).add(c[1]);
+            map.get(c[1]).add(c[0]);
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+        int res = 0;
+        boolean[] vs = new boolean[n];
+        vs[0] = true;
+        while (!q.isEmpty()) {
+            int c = q.poll();
+            for (int next : map.getOrDefault(c, new HashSet<>())) {
+                if (vs[next]) continue;
+                vs[next] = true;
+                if (!st.contains(next + "," + c)) res++;
+                q.offer(next);
+            }
+        }
+        return res;
     }
 }
