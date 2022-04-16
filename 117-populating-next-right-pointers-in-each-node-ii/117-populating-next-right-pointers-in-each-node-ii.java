f83aa637d4b603984620f5a1179b3cee4807a160
Shubham Prakash <43783336+Shub4am@users.noreply.github.com>
2022-04-17 00:58:23 +0530

    Time: 0 ms (100.00%), Space: 42 MB (86.86%) - LeetHub

---
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
@@ -0,0 +1,57 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        Node mostLeft = root;
+        while (mostLeft != null) {
+            Node c = mostLeft;
+            mostLeft = null;
+            Node preRight = null;
+            while(c != null) {
+                if (c.left != null && c.right != null) {
+                    if (preRight != null) {
+                        preRight.next = c.left;
+                    }
+                    c.left.next = c.right;
+                    preRight = c.right;
+                } else if (c.left != null) {
+                    if (preRight != null) {
+                        preRight.next = c.left;
+                    }
+                    preRight = c.left;
+                } else if (c.right != null) {
+                    if (preRight != null) {
+                        preRight.next = c.right;
+                    }
+                    preRight = c.right;
+                }
+                if (mostLeft == null) {
+                    mostLeft = c.left == null ? c.right: c.left;
+                }
+                c = c.next;
+            }
+        }
+        return root;
+    }
+}
