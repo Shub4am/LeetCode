5ea617156b5f3e53dab59d685cd77f86bce6222d
Shubham Prakash <43783336+Shub4am@users.noreply.github.com>
2022-04-20 03:38:27 +0530

    Time: 526 ms (22.31%), Space: 174.8 MB (5.05%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.java
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.java
@@ -1,26 +1,64 @@
 class Solution {
-    public List countSmaller(int[] nums) {
-        List<Integer> sortedAndVisited = new ArrayList<>();
-        int[] arr = new int[nums.length];
-        for(int i = (nums.length-1); i >= 0; i--){
-            arr[i] = binarySearchAndInsert(nums[i], 0, sortedAndVisited.size(), sortedAndVisited);
+    public class Point {
+        private int val;
+        private int idx;
+        private Point(int val, int idx) {
+            this.val = val;
+            this.idx = idx;
         }
-        List<Integer> output = new ArrayList<>();
-        for(int i=0; i < arr.length; i++) {
-            output.add(arr[i]);
-        }
-        return output;
     }
-    public int binarySearchAndInsert(int currentNo, int low, int high, List<Integer> sortedAndVisited){
-        int mid = low + (high - low)/2;
-        if(low >= high){
-            sortedAndVisited.add(mid, currentNo);
-            return mid;
+    public List<Integer> countSmaller(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return new ArrayList<>();
         }
-        if(currentNo <= sortedAndVisited.get(mid)){
-            return binarySearchAndInsert(currentNo, low, mid, sortedAndVisited);
-        }else{
-            return binarySearchAndInsert(currentNo, mid + 1, high, sortedAndVisited);
+        Point[] points = new Point[nums.length];
+        Map<Point, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            points[i] = new Point(nums[i], i);
+            map.put(points[i], 0);
+        }
+        mergeSort(points, 0, nums.length - 1, map);
+        Integer[] temp = new Integer[nums.length];
+        for (Point p : map.keySet()) {
+            temp[p.idx] = map.get(p);
+        }
+        return Arrays.asList(temp);
+    }
+    public void mergeSort(Point[] points, int start, int end, Map<Point, Integer> map) {
+        if (start == end) {
+            return;
+        }
+        int mid = start + (end - start) / 2;
+        mergeSort(points, start, mid, map);
+        mergeSort(points, mid + 1, end, map);
+        List<Point> list = new ArrayList<>();
+        int left = start;
+        int right = mid + 1;
+        int countOfSmallerRight = 0;
+        while (left <= mid && right <= end) {
+            if (points[left].val <= points[right].val) {
+                map.put(points[left], map.get(points[left]) + countOfSmallerRight);
+                list.add(points[left]);
+                left++;
+            } else {
+                countOfSmallerRight++;
+                list.add(points[right]);
+                right++;
+            }
+        }
+        while (left <= mid) {
+            map.put(points[left], map.get(points[left]) + countOfSmallerRight);
+            list.add(points[left]);
+            left++;
+        }
+        while (right <= end) {
+            countOfSmallerRight++;
+            //
+            list.add(points[right]);
+            right++;
+        }
+        for (int i = start; i <= end; i++) {
+            points[i] = list.get(i - start);
         }
     }
 }
