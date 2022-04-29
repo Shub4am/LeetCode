2567027610a35e9a20ee100c8b251668f2b8d058
Shubham Prakash <43783336+Shub4am@users.noreply.github.com>
2022-04-30 00:09:33 +0530

    Time: 43 ms (44.29%), Space: 48.6 MB (44.84%) - LeetHub

---
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        if(s==null || p == null)
+            return s==p;
+        boolean[][] dp = new boolean[s.length()+1][p.length()+1];
+        dp[0][0]=true;
+        for(int row=0, col=1; col<dp[0].length;col++){
+            if(p.charAt(col-1) == '*')
+                dp[row][col]=true;
+            else
+                break;
+            
+        }
+        
+        for(int row=1; row<dp.length;row++){
+            for(int col=1;col<dp[0].length;col++){
+                char str = s.charAt(row-1);
+                char pattn = p.charAt(col-1);
+                if(str == pattn || pattn == '?'){
+                    dp[row][col] = dp[row-1][col-1];
+                } else if(pattn == '*'){
+                    dp[row][col] = dp[row][col-1] || dp[row-1][col];
+                }
+            }
+        }
+        
+        return dp[s.length()][p.length()];
+        
+    }
+}
