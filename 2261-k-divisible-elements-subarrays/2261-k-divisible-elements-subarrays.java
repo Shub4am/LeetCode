bea9932556f0e0e66bdc5a33e65b1419df032132
Shubham Prakash <43783336+Shub4am@users.noreply.github.com>
2022-05-08 14:14:29 +0530

    Time: 35 ms (99.27%), Space: 43.6 MB (98.75%) - LeetHub

--- 2261-k-divisible-elements-subarrays/2261-k-divisible-elements-subarrays.java
+++ 2261-k-divisible-elements-subarrays/2261-k-divisible-elements-subarrays.java
@@ -1,19 +1,19 @@
 class Solution {
-	public int countDistinct(int[] nums, int k, int p) {
-		HashSet<String> hashSet = new HashSet<>();
-		for(int i = 0; i < nums.length; ++i) {
-			int count = 0;
-			StringBuilder sb = new StringBuilder();
-			for(int j = i; j < nums.length; ++j) {
-				if(nums[j] % p == 0)
-					count++;
-                if(count > k)
-					break;
-                sb.append(nums[j] + ",");
-				hashSet.add(sb.toString());
-			}
-		}
-		return hashSet.size();
-	}
-
+    public int countDistinct(int[] nums, int k, int p) {
+        int n = nums.length;
+        Set<Long> hashSet = new HashSet<>(); 
+        for(int i = 0; i < n; i++) {
+            int count = 0;
+            long hashCode = 1;
+            for(int j = i; j < n; j++) {
+                hashCode = 199L * hashCode + nums[j];
+                if(nums[j] % p == 0)
+                    count++;
+                if(count <= k) {
+                    hashSet.add(hashCode);
+                }
+            }
+        }
+        return hashSet.size();
+    }
 }
