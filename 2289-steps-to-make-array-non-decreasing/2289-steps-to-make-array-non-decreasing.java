d9691602c7f3e228053d75cbc4eb9f11fca506cb
Shubham Prakash <43783336+Shub4am@users.noreply.github.com>
2022-06-14 07:09:08 +0530

    Time: 7 ms (99.46%), Space: 58.2 MB (86.85%) - LeetHub

---
+++ 2289-steps-to-make-array-non-decreasing/2289-steps-to-make-array-non-decreasing.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int totalSteps(int[] nums) {
+        int n = nums.length, res = 0, j = -1;
+        int dp[] = new int[n], stack[] = new int[n];
+        for (int i = n - 1; i >= 0; --i) {
+            while (j >= 0 && nums[i] > nums[stack[j]]) {
+                dp[i] = Math.max(++dp[i], dp[stack[j--]]);
+                res = Math.max(res, dp[i]);
+            }
+            stack[++j] = i;
+        }
+        return res;
+    }
+}
