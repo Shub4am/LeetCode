968dcdab5331183636143780789546897f85b350
Shubham Prakash <43783336+Shub4am@users.noreply.github.com>
2022-06-14 07:06:41 +0530

    Time: 146 ms (29.73%), Space: 49 MB (89.19%) - LeetHub

---
+++ 1803-count-pairs-with-xor-in-a-range/1803-count-pairs-with-xor-in-a-range.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countPairs(int[] nums, int low, int high) {
+        return test(nums, high + 1) - test(nums, low);
+    }
+    private int test(int[] nums, int x) {
+        Map<Integer, Integer> count = new HashMap<>(), count2 = new HashMap<>();
+        for (int n : nums)
+            count.put(n, count.getOrDefault(n, 0) + 1);
+        int res = 0;
+        while (x > 0) {
+            for (int k : count.keySet()) {
+                int v = count.get(k);
+                count2.put(k >> 1, count2.getOrDefault(k >> 1, 0) + v);
+                if ((x & 1) > 0)
+                    res += v * count.getOrDefault((x - 1) ^ k, 0);
+            }
+            count = count2;
+            count2 = new HashMap<>();
+            x >>= 1;
+        }
+        return res / 2;
+    }
+}
