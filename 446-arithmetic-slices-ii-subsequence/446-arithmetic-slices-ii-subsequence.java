f6ea75334a58a8ac739f2ebdebd24aa817f1aec5
Shubham Prakash <43783336+Shub4am@users.noreply.github.com>
2022-07-24 15:21:44 +0530

    Time: 389 ms (29.79%), Space: 172.8 MB (8.51%) - LeetHub

--- 446-arithmetic-slices-ii-subsequence/446-arithmetic-slices-ii-subsequence.java
+++ 446-arithmetic-slices-ii-subsequence/446-arithmetic-slices-ii-subsequence.java
@@ -1,17 +1,15 @@
 class Solution {
     public int numberOfArithmeticSlices(int[] nums) {
-        HashMap <Integer,Integer> [] hashMap = new HashMap[nums.length];
-        for(int i = 0; i < nums.length; i++)
-            hashMap[i] = new HashMap <>();
+        int n = nums.length;
         int res = 0;
-        for(int i = 1; i < nums.length; i++){
-            for(int j = 0; j < i; j++){
-                long n = (long)nums[i] - (long)nums[j];
-                if(n <= Integer.MIN_VALUE || n >= Integer.MAX_VALUE) continue;
-                int endingAtj = hashMap[j].getOrDefault((int)n, 0);
-                int endingAti = hashMap[i].getOrDefault((int)n, 0);
-                res += endingAtj;
-                hashMap[i].put((int) n, endingAtj + endingAti + 1);
+        HashMap<Long, Integer>[] dp = new HashMap[n];
+        for (int i = 0; i < n; ++i) dp[i] = new HashMap<>();
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                long diff = (long) nums[i] - (long) nums[j];
+                int cnt = dp[j].getOrDefault(diff, 0);
+                dp[i].put(diff, dp[i].getOrDefault(diff, 0) + cnt + 1);
+                res += cnt;
             }
         }
         return res;
